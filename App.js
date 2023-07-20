7ff0ddb4eb828ccd2049a65232ece64a4c9ace75
Eddie Otudor <edbeeotudor@gmail.com>
2023-07-20 14:23:37 +0100

    Initial setup for data fetching

--- App.js
+++ App.js
@@ -1,22 +1,73 @@
-import React from "react";
-import CurrentWeather from "./src/screens/CurrentWeather";
-import UpcomingWeather from "./src/screens/UpcomingWeather";
-import City from "./src/screens/City";
+import { useState, useEffect } from "react";
+import { ActivityIndicator, View, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Tabs from "./src/components/Tabs";
+import * as Location from "expo-location";
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
-const Tab = createBottomTabNavigator();
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [weather, setWeather] = useState([]);
+  const [lat, setLat] = useState();
+  const [lon, setLon] = useState();
+
+  const handleFetchWeatherData = async () => {
+    try {
+      const res = await fetch(
+        `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}`
+      );
+      const data = await res.json();
+      setWeather(data);
+    } catch (error) {
+      setError("Could not fetch weather");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setError("permission to access location was denied.");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      console.log(location);
+      setLat(location.coords.latitude);
+      setLon(location.coords.longitude);
+      await handleFetchWeatherData();
+    })();
+  }, [lat, lon]);
+
+  if (weather) {
+    console.log(weather);
+  }
+
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size={"large"} color={"blue"} />
+      </View>
+    );
+  }
+
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name={"Current"} component={CurrentWeather} />
-        <Tab.Screen name={"Upcoming"} component={UpcomingWeather} />
-        <Tab.Screen name={"City"} component={City} />
-      </Tab.Navigator>
+      <Tabs />
     </NavigationContainer>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+});
 
 export default App;
